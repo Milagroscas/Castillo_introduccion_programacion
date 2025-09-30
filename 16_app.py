d43b23892b93c4935eabce914c005f9e9d0ddae9
Milagroscas <i202518851@cibertec.edu.pe>
2025-09-30 02:39:08 +0000

    Primer ejemplo con bucle For

---
+++ 16_app.py
@@ -0,0 +1,13 @@
+# Estructura de bucle For
+
+# Sintaxis
+# for value in sequence:
+#   instrucción, que es lo que debe hacer mientras se cumple el bucle
+
+# Ejemplo:
+
+# Listado de paises
+paises = ["venezuela", "colombia", "peru", "suiza", "argentina"]
+
+for pais in paises:
+    print(pais)
